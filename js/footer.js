9c1cf19a282920af9ea05c1b8ecfeeb98f670ba8
Tuan Phu <2051010343tuan@ou.edu.vn>
2023-05-26 19:49:17 +0700

    first commit

---
+++ js/footer.js
@@ -0,0 +1,57 @@
+const footer = document.querySelector('.footer')
+
+
+footer.innerHTML += `
+<div class="box-info col-sm-6 col-12 col-md-4">
+<div class="title-footer">THÔNG TIN LIÊN HỆ</div>
+<div class="name-company">CÔNG TY TNHH  XÂY DỰNG VÀ THIẾT KẾ NỘI THẤT PHAN THANH</div>
+<div class="icon-internet">
+  <a href="https://www.facebook.com/Phuong.Lotusarch"
+    ><img src="./images/icon-facebook.svg" alt="" width="40px"
+  /></a>
+  <a href=""
+    ><img src="./images/icon-intagram.svg" alt="" width="40px"
+  /></a>
+  <a href=""
+    ><img src="./images/icon-pinterest.svg" alt="" width="40px"
+  /></a>
+  <a href=""
+    ><img src="./images/icon-youtube.svg" alt="" width="40px"
+  /></a>
+  <a href="https://zalo.me/0906330614"><img src="./images/icon-zalo.svg" alt="" width="40px" /></a>
+</div>
+<p> <i class="fa fa-envelope" aria-hidden="true"></i>noithatphanthanh@gmail.com</p>
+<p> <i class="fa fa-phone" aria-hidden="true"></i>Hotline:0933366630</p>
+<p> <i class="fa fa-clock-o" aria-hidden="true"></i>Thời gian làm việc 8:00 - 17:00 (Th2 - Th7)</p>
+
+
+</div>
+<div class="box-support col-sm-6 col-12 col-md-4">
+<div class="title-footer">HỖ TRỢ KHÁCH HÀNG</div>
+<div class="support">
+  <p> <i class="fa fa-hand-o-right" aria-hidden="true"></i>Quy trình làm việc</p>
+  <p><i class="fa fa-hand-o-right" aria-hidden="true"></i>Bảo hành đổi trả</p>
+  <p><i class="fa fa-hand-o-right" aria-hidden="true"></i>Hình Thức Thanh Toán</p>
+  <p><i class="fa fa-hand-o-right" aria-hidden="true"></i>Vận Chuyển - Giao Nhận </p>
+  <p><i class="fa fa-hand-o-right" aria-hidden="true"></i>Chính Sách Bảo Mật</p>
+</div>
+<div class="map">
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3920.196359097493!2d106.63033676917837!3d10.719332936901576!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752e092ac15d6b%3A0x9351d1ffc6b24e35!2zMjg4OUEvMjFBIFBo4bqhbSBUaOG6vyBIaeG7g24sIFBoxrDhu51uZyA3LCBRdeG6rW4gOCwgVGjDoG5oIHBo4buRIEjhu5MgQ2jDrSBNaW5oLCBWaeG7h3QgTmFt!5e0!3m2!1svi!2s!4v1685096068123!5m2!1svi!2s" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</div>
+
+</div>
+<div class="box-room col-sm-6 col-12 col-md-4">
+
+<div class="diachi">
+  <h5 class="tencongty">CÔNG TY TNHH  XÂY DỰNG VÀ THIẾT KẾ NỘI THẤT PHAN THANH</h5>
+<p>Số 2889A/21A, Đường Phạm Thế Hiển, Tổ 26, Khu Phố 2, Phường 7, Quận 8, Thành Phố Hồ Chí Minh</p>
+<p>MST : 0317826772 </p>
+<p>Điện thoại:  0933366630</p>
+<p>Đại diện:  PHAN THANH VÂN  .   Chức vụ: Giám Đốc</p>
+</div>
+<div class="img-dangki">
+<img src="./images/14971568174511_da-thong-bao-bo-cong-thuong.png" alt="" width="200px">
+</div>
+</div>
+
+`
